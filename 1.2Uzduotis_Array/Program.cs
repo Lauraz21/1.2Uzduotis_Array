cb2e21da3b0027273168c5f266f45f2f3151098c
Laura <Laura@DESKTOP-BESSB6E>
2023-09-18 21:10:17 +0300

    Add project files.

---
+++ 1.2Uzduotis_Array/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int[] numbers = { 1, 2, 3, 4, 5 };
+        int result = SumOfNumbers(numbers);
+        Console.WriteLine(result);
+    }
+
+    public static int SumOfNumbers(int[] numbers)
+    {
+        int sum = 0;
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            sum += numbers[i];
+        }
+        return sum;
+    }
+}
